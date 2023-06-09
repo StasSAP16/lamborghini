e43dd55b84cd4426ebf584a3f932afb4010223cb
Stanislav <amirov202@gmail.com>
2023-06-09 13:33:28 +0300

    @media /header/promo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+const menuBtn = document.querySelector('.menu-btn');
+const menuMobile = document.querySelector('.menu-mobile');
+
+menuBtn.addEventListener('click', () => {
+    menuMobile.classList.toggle('menu--open');
+});
+
 const swiper = new Swiper(".swiper", {
   // комментируем вертикальный слайдер тк нам нужен горизонтальный
   // direction: 'vertical',
